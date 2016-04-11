3c448e462ececa2a7f120ec7713fc434e09ae697
陈秋远 <380133194@qq.com>
2016-04-11 09:59:41 +0800

    include my code in a vcs

--- auto_send.py
+++ auto_send.py
@@ -1,10 +1,8 @@
-try:
-    import shibor_csv_file
-    import qq_mail
-except ImportError:
-    from mpython.send_mail import shibor_csv_file
-    from mpython.send_mail import qq_mail
+#-*- encoding:utf-8 -*-
+import qq_mail
+import shibor_csv_file
 
 shibor_csv_file.file()
 qq_mail.send('380133194@qq.com')
-
+# qq_mail.send('henry.duye@gmail.com')
+# qq_mail.send('ycbillows@qq.com')
--- qq_mail.py
+++ qq_mail.py
@@ -4,10 +4,12 @@
 import datetime
 import smtplib
 from email.mime.image import MIMEImage
+
 import email.MIMEBase
 import email.MIMEMultipart
 import email.MIMEText
-import os
+
+from test_code import os
 
 
 def addimg(src,imgid):
@@ -54,7 +56,7 @@ def send(TO):
         server = smtplib.SMTP()
         server.connect(HOST,"25")
         server.starttls()
-        server.login("380133194@qq.com","password")
+        server.login("380133194@qq.com","caffvykckhbtbheb")
         server.sendmail(FROM, TO, main_msg.as_string())
         server.quit()
         print "邮件发送成功！"
