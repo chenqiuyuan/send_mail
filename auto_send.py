200d7c1b33682c30e1e81f9c9a49a4e6e66a263a
陈秋远 <380133194@qq.com>
2016-05-11 16:06:40 +0800

    Merge remote-tracking branch 'origin/master'

--- auto_send.py
+++ auto_send.py
@@ -4,5 +4,5 @@ import shibor_csv_file
 
 shibor_csv_file.file()
 qq_mail.send('380133194@qq.com')
-# qq_mail.send('henry.duye@gmail.com')
-# qq_mail.send('ycbillows@qq.com')
+qq_mail.send('henry.duye@gmail.com')
+qq_mail.send('ycbillows@qq.com')
